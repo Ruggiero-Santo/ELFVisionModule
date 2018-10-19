d6c6db04bdb7a2b0e3d3adeb1b2a70b7eea081eb
Domenico Tortorella <dom.tortorella@gmail.com>
2018-10-19 14:22:06 +0200

    Draft offline prototype with face matching

---
+++ offline-prototype/prototype.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+import cv2
+import dlib
+import numpy as np
+import uuid
+
+cam = cv2.VideoCapture(0)
+detector = dlib.get_frontal_face_detector()
+shape_pred = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+face_rec = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
+people = {}
+descriptor_distance_threshold = 0.5
+
+while True:
+    _, frame = cam.read()
+    detected_faces = detector(frame, 1)
+    for rect in detected_faces:
+        shape = shape_pred(frame, rect)
+        face_desc = face_rec.compute_face_descriptor(frame, shape)
+        face_desc = np.array(face_desc)
+        max_distance = float('inf')
+        match_id = None
+        for person_id, person_desc in people.items():
+            distance = np.linalg.norm(person_desc - face_desc)
+            if distance < max_distance:
+                max_distance = distance
+                match_id = person_id
+        if max_distance > descriptor_distance_threshold:
+            match_id = str(uuid.uuid4())
+            people[match_id] = face_desc
+        cv2.rectangle(frame, (rect.left(), rect.top()), (rect.right(), rect.bottom()), (0, 255, 0))
+        cv2.putText(frame, match_id, (rect.left(), rect.top()), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
+    cv2.imshow('cam', frame)
+    cv2.waitKey(1)
