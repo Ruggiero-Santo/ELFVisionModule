5cbb30e6c32cf10079f6cf8eac406296ba47c7cf
michele <mikele.c@outlook.it>
2018-10-31 17:17:30 +0100

    add test package

---
+++ tests/test_Facepp.py
@@ -0,0 +1,13 @@
+import unittest
+import cv2
+from .API_Class import FacePlusPlus
+
+class Test(unittest.TestCase):
+
+    def test1():
+        print("CACCA")
+
+        return
+
+if __name__ == '__main__':
+    Test()
