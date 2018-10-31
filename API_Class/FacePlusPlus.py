c5d5925c92c87f997d875ed203fd1ba61cc0ee50
Ruggiero-Santo <ruggiero.santo@gmail.com>
2018-10-31 14:05:18 +0100

    Added rectangle and landmarks of the recognited faces

--- API_Class/FacePlusPlus.py
+++ API_Class/FacePlusPlus.py
@@ -1,13 +1,28 @@
 import os
 import cv2
+try:
+    import json
+except ImportError:
+    import simplejson as json
 
 class FacePlusPlus():
 
     def _drawRectFace(image, jsonResult):
         faces = jsonResult["faces"]
+        img_h, img_w, _ = image.shape
+        size = (img_w, img_h)
         for face in faces:
-            print(face["face_rectangle"])
-            # w, x, y, h = face["face_rectangle"].values()
+            # Draw Rectangle
+            w, y, x, h = tuple(map(abs, face["face_rectangle"].values()))
+            end_face = tuple(map(lambda x, y: x+y, (x, y), (w, h)))
+            cv2.rectangle(image, (x, y), end_face, (0,255,0), 3)
+
+            # Draw Landmarks
+            fl = face["landmark"]
+            for key, pos in fl.items():
+                xy = (int(pos["x"]), int(pos["y"]))
+                cv2.circle(image, xy, 1, (0,0,255), -1)
+
         return image
 
     def __init__(self, FACEpp_Key = None, FACEpp_Secret = None):
@@ -21,7 +36,7 @@ class FacePlusPlus():
             self.url = 'https://api-eu.faceplusplus.com/facepp/v3/detect'
 
     def initializer(self):
-        self.url_params.update({"return_attributes": "gender,age,smiling,headpose,facequality,blur,eyestatus,emotion,ethnicity,beauty,mouthstatus,eyegaze,skinstatus"})
+        self.url_params.update({"return_attributes": "gender,age,smiling,headpose,facequality,blur,eyestatus,emotion,ethnicity,beauty,mouthstatus,eyegaze,skinstatus", "return_landmark": 2})
 
 
     def caller(self, frame):
@@ -29,7 +44,7 @@ class FacePlusPlus():
         _files = {'image_file': cv2.imencode('.jpg', frame)[1]}
 
         response = requests.post(self.url, params = self.url_params, files = _files)
-        frame = FacePlusPlus._drawRectFace(frame, response.text)
+        frame = FacePlusPlus._drawRectFace(frame, json.loads(response.text))
         print(response.text)
         return frame
 
--- API_Class/__init__.py
+++ API_Class/__init__.py
@@ -7,4 +7,4 @@ from .FacePlusPlus import *
 
 __all__ = ["openCV", "Azure", "SkyBiometry", "FacePlusPlus"]
 __version__ = "1.0"
-__author__ = "Ruggiero Santo <ruggiero.santo@gmail.com>"
+__author__ = "Ruggiero Santo <ruggiero.santo@gmail.com>, Michele Cafagna <mikele.c@outlook.it>"
