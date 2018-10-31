b743ab2e0a2b652fdb2c3818a4243b5222ab00ed
michele <mikele.c@outlook.it>
2018-10-31 20:55:15 +0100

    Added test_faces_only

--- API_Class/FacePlusPlus.py
+++ API_Class/FacePlusPlus.py
@@ -1,29 +1,20 @@
 import os
 import cv2
+import requests
 try:
     import json
 except ImportError:
     import simplejson as json
 
+"""
+Image Requirements
+Format : JPG (JPEG), PNG
+Size : between 48*48 and 4096*4096 (pixels)
+File size : no larger than 2MB
+Minimal size of face : the bounding box of a detected face is a square. The minimal side length of a square should be no less than 1/48 of the short side of image, and no less than 48 pixels. For example if the size of image is 4096 * 3200px, the minimal size of face should be 66 * 66px.
+"""
+
 class FacePlusPlus():
-
-    def _drawRectFace(image, jsonResult):
-        faces = jsonResult["faces"]
-        img_h, img_w, _ = image.shape
-        size = (img_w, img_h)
-        for face in faces:
-            # Draw Rectangle
-            w, y, x, h = tuple(map(abs, face["face_rectangle"].values()))
-            end_face = tuple(map(lambda x, y: x+y, (x, y), (w, h)))
-            cv2.rectangle(image, (x, y), end_face, (0,255,0), 3)
-
-            # Draw Landmarks
-            fl = face["landmark"]
-            for key, pos in fl.items():
-                xy = (int(pos["x"]), int(pos["y"]))
-                cv2.circle(image, xy, 1, (0,0,255), -1)
-
-        return image
 
     def __init__(self, FACEpp_Key = None, FACEpp_Secret = None):
         key = os.getenv('FACEpp_KEY', None) if FACEpp_Key==None else FACEpp_Key
@@ -40,13 +31,43 @@ class FacePlusPlus():
 
 
     def caller(self, frame):
-        import requests
         _files = {'image_file': cv2.imencode('.jpg', frame)[1]}
 
         response = requests.post(self.url, params = self.url_params, files = _files)
-        frame = FacePlusPlus._drawRectFace(frame, json.loads(response.text))
+        frame = drawRectFace(frame, json.loads(response.text))
         print(response.text)
         return frame
 
+    def detect(self, frame = None, image = None):
+        self.url = 'https://api-eu.faceplusplus.com/facepp/v3/detect'
+        self.url_params.update({"return_attributes": "gender,age,smiling,emotion"})
+
+        if frame is not None:
+            data = cv2.imencode('.jpg', frame)[1]
+
+        if image is not None:
+            data = image
+
+        return json.loads(requests.post(self.url, params = self.url_params, files = {'image_file': data}).text)
+
     def finalizer(self):
         pass
+
+
+def drawRectFace(image, jsonResult):
+    faces = jsonResult["faces"]
+    img_h, img_w, _ = image.shape
+    size = (img_w, img_h)
+    for face in faces:
+        # Draw Rectangle
+        w, y, x, h = tuple(map(abs, face["face_rectangle"].values()))
+        end_face = tuple(map(lambda x, y: x+y, (x, y), (w, h)))
+        cv2.rectangle(image, (x, y), end_face, (0,255,0), 3)
+
+        # Draw Landmarks
+        fl = face["landmark"]
+        for key, pos in fl.items():
+            xy = (int(pos["x"]), int(pos["y"]))
+            cv2.circle(image, xy, 1, (0,0,255), -1)
+
+    return image
--- tests/test_Facepp.py
+++ tests/test_Facepp.py
@@ -1,13 +1,25 @@
 import unittest
 import cv2
-from .API_Class import FacePlusPlus
+import os
+from API_Class import FacePlusPlus
+
 
 class Test(unittest.TestCase):
 
-    def test1():
-        print("CACCA")
+    @classmethod
+    def setUpClass(self):
+        self.client = FacePlusPlus()
+        self.client.initializer()
+        self.currdir = os.getcwd()
 
+    def test_face_frame_only(self):
+        #face_path = os.path.join(self.currdir, "faces")
+        face_path = "tests/faces"
+
+        files = [os.path.join(face_path, f) for f in os.listdir(face_path) if os.path.isfile(os.path.join(face_path, f))]
+
+        for file in files:
+            json = self.client.detect(image = open(file, 'rb'))
+
+            self.assertEqual(json.get('error_message'), None)
         return
-
-if __name__ == '__main__':
-    Test()
