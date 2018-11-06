f99104a4a1d10f55b6d9fff39956cfe1344db5ac
Ruggiero-Santo <ruggiero.santo@gmail.com>
2018-11-06 20:23:58 +0100

    Added search, removeFace

--- API_Class/SDK/face_plusplus_client/face_client.py
+++ API_Class/SDK/face_plusplus_client/face_client.py
@@ -1,6 +1,6 @@
+import os
 import cv2
 import requests
-import os
 try:
     import json
 except ImportError:
@@ -54,6 +54,49 @@ class Facepp_Client(object):
 
         r = requests.post(url, params = self.url_params, files = {'image_file': data})
         return json.loads(r.text)
+
+    def search(self, face_token = None, image_url = None, image_file = None, image_base64 = None, faceset_token = None, outer_id = None, return_result_count = 1):
+        url = API_HOST + "search"
+        params = self.url_params
+        file = None
+
+        if not face_token and not image_url and image_file is None and not image_base64:
+            raise AttributeError('Missing face_token or image_url or image_file or image_base64 argument. At least one must be set.')
+
+        if face_token:
+            if isinstance(face_token, str):
+                params.update({'face_token': face_token})
+            else:
+                raise AttributeError("face_token must be a string.")
+        elif image_url:
+            if isinstance(image_url, str):
+                params.update({'image_url': image_url})
+            else:
+                raise AttributeError("face_token must be a string.")
+        elif not image_file is None:
+            if isinstance(image_file, str):
+                try:
+                    file = {'image_file': open(file, 'rb')}
+                except OSError:
+                    raise AttributeError("There was an error during opening file, check if path is valid.")
+            else:
+                file = {'image_file': image_file}
+        else:
+            raise AttributeError("image_base64 not yet implement.")
+
+        if outer_id:
+            params.update({'outer_id': outer_id})
+        elif faceset_token:
+            params.update({'faceset_token': faceset_token})
+        else:
+            raise AttributeError('You must define a unique outer_id or face_token.')
+
+        if return_result_count <= 0 or return_result_count > 5:
+            raise AttributeError('return_result_count can be between [1,5]. The default value is 1.')
+        else:
+            params.update({'return_result_count': return_result_count})
+
+        return json.loads(requests.post(url, params = params, files = file).text)
 
     def deleteFaceSet(self, outer_id = None, faceset_token = None ):
         url = API_HOST + 'faceset/delete'
@@ -116,7 +159,36 @@ class Facepp_Client(object):
         elif faceset_token:
             params.update({'faceset_token': faceset_token})
         else:
-            raise AttributeError('You must define a unique outer_id  or  face_token.')
+            raise AttributeError('You must define a unique outer_id or face_token.')
+
+        return json.loads(requests.post(url, params = params).text)
+
+    def removeFace(self, face_tokens, faceset_token = None, outer_id = None):
+        """
+            params:
+                face_tokens(str or list(str)):
+                    if this string passed "RemoveAllFaceTokens", all the face_token within FaceSet will be removed.
+        """
+        url = API_HOST + "faceset/removeface"
+        params = self.url_params
+
+        if face_tokens:
+            if isinstance(face_tokens, list):
+                if len(face_tokens) < 1000:
+                    params.update({'face_tokens': ",".join(face_tokens)})
+                else:
+                    raise AttributeError('face_tokens array must be length at most 1000.')
+            elif isinstance(face_tokens, str):
+                params.update({'face_tokens': face_tokens})
+            else:
+                raise AttributeError('face_tokens should be a string or a list of string. You provided a ' + type(face_tokens).__name__ + 'instead.')
+
+        if outer_id:
+            params.update({'outer_id': outer_id})
+        elif faceset_token:
+            params.update({'faceset_token': faceset_token})
+        else:
+            raise AttributeError('You must define a unique outer_id or face_token.')
 
         return json.loads(requests.post(url, params = params).text)
 
