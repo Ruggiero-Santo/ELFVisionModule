9e39cc68fc013f2bf562de53e3bd9c2689b28bb1
michele <mikele.c@outlook.it>
2018-11-06 16:11:00 +0100

    minor fixes

--- API_Class/openCV.py
+++ API_Class/openCV.py
@@ -6,7 +6,7 @@ class openCV():
     def __init__(self):
         self.face = cv2.CascadeClassifier(cv2.data.haarcascades+'/haarcascade_frontalface_default.xml')
 
-    def caller(self, frame):
+    def simple_demo(self, frame):
         faces = self.face.detectMultiScale(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
 
         # Draw a rectangle around the faces
--- tests/test_Facepp.py
+++ tests/test_Facepp.py
@@ -24,12 +24,21 @@ class Test(unittest.TestCase):
 
     def test_detect_with_path(self):
         face_path = "tests/faces"
-
         files = [os.path.join(face_path, f) for f in os.listdir(face_path) if os.path.isfile(os.path.join(face_path, f))]
 
         for file in files:
             json = self.client.detect(file = file)
             self.assertEqual(json.get('error_message'), None)
+        return
+
+    def test_detect_different_attributes(self):
+        face_path = "tests/faces"
+        files = [os.path.join(face_path, f) for f in os.listdir(face_path) if os.path.isfile(os.path.join(face_path, f))]
+
+        json = self.client.detect(file = files[0], attributes = 'gender,age,emotion')
+        self.assertTrue('emotion' in json['faces'][0]['attributes'])
+        self.assertTrue('age' in json['faces'][0]['attributes'])
+        self.assertTrue('gender' in json['faces'][0]['attributes'])
         return
 
 
